cab5e3767315df4b14818fa6833cd6eb2d9ad708
gundam <zhangweipengphp@163.com>
2019-03-31 16:22:40 +0800

    update

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,7 +15,7 @@ class TestController extends Controller
 {
     public function test()
     {
-        return $this->testPing();
+        return $this->testBitmexQuery();
     }
 
     public function testPing()
@@ -36,5 +36,50 @@ class TestController extends Controller
         } catch (Exception $e) {
             echo '[Error] ' . $e->getMessage () . "\n";
         }
+    }
+
+    public function testBitmexQuery()
+    {
+        $exchange = new \ccxt\bitmex(array (
+            // 'verbose' => true, // for debugging
+            'timeout' => 30000,
+        ));
+
+        try {
+
+            $result = $exchange->fetch_tickers ();
+
+            dd ($result);
+
+        } catch (\ccxt\NetworkError $e) {
+            echo '[Network Error] ' . $e->getMessage () . "\n";
+        } catch (\ccxt\ExchangeError $e) {
+            echo '[Exchange Error] ' . $e->getMessage () . "\n";
+        } catch (Exception $e) {
+            echo '[Error] ' . $e->getMessage () . "\n";
+        }
+    }
+
+    public function testBitmexOrder()
+    {
+        $exchange = new \ccxt\bitmex (array (
+            'apiKey' => 'YOUR_API_KEY', // ←------------ replace with your keys
+            'secret' => 'YOUR_SECRET',
+            'enableRateLimit' => true,
+        ));
+
+        $symbol = 'XBTM18'; // bitcoin contract according to bitmex futures coding
+        $type = 'StopLimit'; // # or 'market', or 'Stop' or 'StopLimit'
+        $side = 'sell'; // or 'buy'
+        $amount = 1.0;
+        $price = 6500.0; // or None
+
+        // extra params and overrides
+        $params = array (
+            'stopPx' => 6000.0, // if needed
+        );
+
+        $order = $exchange->create_order ($symbol, $type, $side, $amount, $price, $params);
+
     }
 }
